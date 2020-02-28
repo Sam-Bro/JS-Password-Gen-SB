129c4c3d7c2411ef564e418cb6bc1e2211a38a1f
OrbmasWin <sambrostudent@gmail.com>
2020-02-28 21:01:22 +0800

    added selectors and variables

---
+++ passgen.js
@@ -0,0 +1,8 @@
+//variables and prompts
+const results = document.getElementById("result");
+var characters = prompt("How many characters should the password be? (8-128)");
+var upperCase = prompt("Should the password contain uppercase letters? (yes / no)");
+var lowerCase = prompt("Should the password contain lowercase letters? (yes / no)");
+var numbers = prompt("Should the password contain numbers? (yes / no)");
+var symbols = prompt("Should the password contain symbols? (yes / no)")
+
