e969f6e3a3f4fcb3965a214f2350ab35e96cce80
OrbmasWin <sambrostudent@gmail.com>
2020-02-28 21:23:45 +0800

    added math.random function

--- passgen.js
+++ passgen.js
@@ -50,3 +50,7 @@ if (symbols.toLowerCase() === 'yes') {
 }else {
     var userSymbols = false;
 }
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
